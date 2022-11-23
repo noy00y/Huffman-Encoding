90e349c570cbfcfe6755ba6f10463a1d6df1a04a
noy00y <69055108+noy00y@users.noreply.github.com>
2022-11-23 05:58:52 -0500

    initial setup

---
+++ encode.py
@@ -0,0 +1,12 @@
+"""
+Processing:
+- Read inputfile
+- Convert whitespace characters to regular space
+- Convert uppercase to lowercase
+- ignore all characters except for A-Z, a-z, comma, period, whitespace
+"""
+
+"""
+Character Frequency:
+- 
+"""
