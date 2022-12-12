bf607f963b3e24699022c686d169e3af763b74d6
noy00y <69055108+noy00y@users.noreply.github.com>
2022-12-11 21:45:16 -0500

    the pq finally works

--- encode.cpp
+++ encode.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <iterator>
 #include <typeinfo>
+#include <queue>
 using namespace std;
 
 // Public Stuff:
@@ -16,19 +17,34 @@ vector<char> alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n'
 vector<char> numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 map<char, int> frequencies;
 
-// Structures:
-struct Node {
-    char data; 
-    int frequency;
-    string binary_code;
-    struct Node *left, *right;
+// Class:
+class Node {
+public:
+    char data; // leaf node 
+    int frequency; // frequency of node
+    string binary_code; // leaf node
+    Node *right, *left;
 
-    // Constructor
-    Node(int freq) {
-        frequency = freq;
-    }  
+public:
+    // Constructor:
+    Node(int f) {
+        frequency = f;
+    }
+    void set_data(char d) {
+        data = d;
+    }
+    void set_code(string c) {
+        binary_code = c;
+    }
 };
 
+class Compare {
+public:
+    int operator() (const Node& p1, const Node& p2)
+    {
+        return p2.frequency < p1.frequency;
+    }
+};
 
 
 // Functions:
@@ -104,7 +120,8 @@ void set_lines(string line, string file_name) {
 
 // Driver Code:
 int main(){
-    string file_name = "input.txt"; // Declarations:
+    // Declarations
+    string file_name = "input.txt"; // Input File name
     size_check(file_name);// Check File Size:
     vector<string> lines = get_lines(file_name); // Get Lines from File
 
@@ -122,15 +139,30 @@ int main(){
         frequencies[numbers[i]] = 0;
     }
 
-    // Create filtered copy of text
     vector<char> copy = create_lines(lines); // using this char copy we will create a bin file from the converted accii characters (binary)
+
+    priority_queue <Node, vector<Node>, Compare> heap; // Create Heap
+    // Create filtered copy of text:
     for (itr = frequencies.begin(); itr != frequencies.end(); ++itr) { // Loop and add to file
-        string c(1, itr->first);
-        string n = to_string(itr->second);
+        string c(1, itr->first); // character
+        string n = to_string(itr->second); // frequency
+
+        // Create node:
+        Node node = Node(itr->second);
+        node.data = itr->first;
+
+        // cout << node.data << ": " << node.frequency << endl;
+        heap.push(node);
+
+        // Print to frequency file
         set_lines(c + ":" +  n, "frequency.txt");
     }    
-
-    // 
-
-    return 0;
+    // cout << "------------------------------------"<< endl;
+    while (heap.empty() == false)
+    {
+        Node p = heap.top();
+        cout << "(" << p.data << ": " << p.frequency << ")";
+        cout << endl;
+        heap.pop();
+    }
 }
