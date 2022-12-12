6f34ff9b26da5f74b2cb53c07930c8460b44cd1d
noy00y <69055108+noy00y@users.noreply.github.com>
2022-12-11 19:09:08 -0500

    fixing bug in frequeny

--- encode.cpp
+++ encode.cpp
@@ -12,6 +12,8 @@ using namespace std;
 // Public Stuff:
 vector<char> convert = {'\t', '\v', '\r', '\n'};
 vector<char> other = {'.', ',', ' '};
+vector<char> alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+vector<char> numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 map<char, int> frequencies;
 
 // Functions:
@@ -73,7 +75,6 @@ vector<char> create_lines(vector<string> lines) {
             // Check Ignored
             // else {cout << line[q];}
         }
-        // cout << lines[i] << endl;
     }
     return copy;
 }
@@ -91,16 +92,29 @@ int main(){
     string file_name = "input.txt"; // Declarations:
     size_check(file_name);// Check File Size:
     vector<string> lines = get_lines(file_name); // Get Lines from File
-    vector<char> copy = create_lines(lines); // using this char copy we will create a bin file from the converted accii characters (binary)
-
 
     // Create frequencies.txt:
     map<char, int>::iterator itr;
-    for (itr = frequencies.begin(); itr != frequencies.end(); ++itr) {
+    vector<string> frequency_lines;
+
+    // Initialize the frequencies:
+    for (int i = 0; i < other.size(); i++) {
+        frequencies[other[i]] = 0;
+    }
+    for (int i = 0; i < alphabet.size(); i++) {
+        frequencies[alphabet[i]] = 0;
+    }   
+    for (int i = 0; i < numbers.size(); i++) {
+        frequencies[numbers[i]] = 0;
+    }
+    vector<char> copy = create_lines(lines); // using this char copy we will create a bin file from the converted accii characters (binary)
+    for (itr = frequencies.begin(); itr != frequencies.end(); ++itr) { // Loop and add to file
         string c(1, itr->first);
         string n = to_string(itr->second);
-        set_lines(c + ": " +  n, "frequency.txt");
+        set_lines(c + ":" +  n, "frequency.txt");
     }    
+
+    // 
 
     return 0;
 }
