53d2c9542f8e76f615cfee08afb4513615ea0400
noy00y <69055108+noy00y@users.noreply.github.com>
2022-12-12 05:19:03 -0500

    at least the tree works now :)

--- encode.cpp
+++ encode.cpp
@@ -17,32 +17,120 @@ vector<char> alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n'
 vector<char> numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 map<char, int> frequencies;
 
-// Structures:
+// Structures and their methods:
 struct Node {
     char data;
     int frequency;
     string binary_code;
     struct Node *right, *left;
-
-    // Constructor:
-    Node(char d, int f) {
-        data = d;
-        frequency = f;
-        right = NULL;
-        left = NULL;
-    }
-
-    Node(int f) {
-        frequency = f;
-        right = NULL;
-        left = NULL;
-    }
-
-    // Comparison Operator for Queue
-    bool operator<(const Node& node) const {return frequency > node.frequency;} 
 };
 
+Node *create_node(int frequency) {
+    Node *node = (Node*)malloc(sizeof(Node));
+    node->frequency = frequency;
+    node->right = node->left = NULL;
+    return node;
+}
+
+struct Heap {
+    int size;
+    int capacity;
+    struct Node** array;
+};
+
+Heap *create_heap(int capaacity) {
+    Heap *heap = (Heap*)malloc(sizeof(Heap));
+    heap->size = 0;
+    heap->capacity = capaacity;
+    heap->array = (struct Node**)malloc(heap->capacity * sizeof(Node*));
+    return heap;
+}
+
+
 // Functions:
+void swap(Node** a, Node** b) {
+    Node* temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+void heapify(Heap *heap, int index) {
+    int smallest = index;
+    int left = 2 * index + 1; // get left node
+    int right = 2 * index + 2; // get right node
+ 
+    // If left child smaller then "smallest" node --> set left child to smallest
+    if (left < heap->size && heap->array[left]->frequency < heap->array[smallest]->frequency) {smallest = left;}
+ 
+    // If right child smaller then "smallest" node --> set right child to smallest
+    if (right < heap->size && heap->array[right]->frequency < heap->array[smallest]->frequency) {smallest = right;}
+ 
+    // If root node is not the smallest --> swap with smallest
+    if (smallest != index) {
+        swap(&heap->array[smallest], &heap->array[index]);
+        heapify(heap, smallest); // heapify again with new root
+    }
+    return;
+}
+
+void insert(Heap *heap, Node *node) {
+    ++heap->size; // increment heap size
+    int i = heap->size - 1; // index --> leaf node
+    // While node frequenecy less then parent node frequency
+    while (i && node->frequency < heap->array[(i - 1) / 2]->frequency) {
+        heap->array[i] = heap->array[(i - 1) / 2]; // set parent as leaf index
+        i = (i - 1) / 2; // change index
+    }
+    heap->array[i] = node; // set node at index
+    return;
+}
+
+void build_heap(Heap *heap) {
+    int n = heap->size - 1;
+    int i;
+
+    // Heapify at each parent node
+    for (i = (n - 1) / 2; i >= 0; --i) {
+        heapify(heap, i);
+    }
+    return;
+}
+
+Node *pop(Heap *heap) {
+    Node* node = heap->array[0]; // get min node from heap
+    heap->array[0] = heap->array[heap->size - 1]; // set root as a leaf node
+    --heap->size; // Decrement size of heap
+    heapify(heap, 0);
+    return node;
+}
+
+Node *build_tree(Heap *heap) {
+    Node *left, *right, *root;
+    while (heap->size > 1) {
+        left = pop(heap);
+        right = pop(heap);
+        root = create_node(left->frequency + right->frequency);
+        root->left = left;
+        root->right = right;
+        insert(heap, root);
+    }
+    return pop(heap);
+}
+
+void create_codes(Node *root) {
+    if (root == NULL) {
+        return;
+    }
+    printTree(root->left);
+    cout << "Node Frequency: " << root->frequency << endl;
+    if (!(root->left) && !(root->right)) {
+        cout << "Node Data --> " << root->data << ": " << root->frequency << endl;
+    }
+    printTree(root->right);
+}
+
+// Utils:
 void size_check(string file_name) {
     ifstream file(file_name, ios::binary);
     file.seekg(0, ios::end);
@@ -111,29 +199,6 @@ void set_lines(string line, string file_name) {
     return;
 }
 
-Node build_tree(priority_queue <Node> heap) {
-    cout << "build tree" << endl;
-    while (heap.size() > 1) {
-        Node left = heap.top(); heap.pop();
-        Node right = heap.top(); heap.pop();
-        Node root = {left.frequency + right.frequency};
-        root.left = &left;
-        root.right = &right;
-        cout << "Root: " << root.frequency;
-        cout << ", Right: " << right.data;
-        cout << ", Left: " << left.data << endl;
-        heap.push(root);
-    }
-    Node root = heap.top(); heap.pop();
-    cout << "Final Root: " << root.frequency << endl;
-    return root;
-}
-
-void printTree(Node node) {
-    cout << node.right->frequency << endl;
-    return;
-}
-
 // Driver Code:
 int main(){
     // Declarations
@@ -156,28 +221,23 @@ int main(){
     }
 
     vector<char> copy = create_lines(lines); // using this char copy we will create a bin file from the converted accii characters (binary)
-
-    priority_queue <Node> heap; // Create Heap
     // Create filtered copy of text:
+    Heap *heap = create_heap(39);
+    int counts = 0;
     for (itr = frequencies.begin(); itr != frequencies.end(); ++itr) { // Loop and add to file
         string c(1, itr->first); // character
         string n = to_string(itr->second); // frequency
 
         // Create node:
-        Node node = {itr->first, itr->second};
-        // cout << node.data << ": " << node.frequency << endl;
-        heap.push(node);
-        // Print to frequency file
-        // set_lines(c + ":" +  n, "frequency.txt");
-    }    
+        heap->array[counts] = create_node(itr->second);
+        heap->array[counts]->data = itr->first;
+        ++counts;
+    }
+    heap->size = 39;
+    build_heap(heap);    
     cout << "--------------------------------------------" << endl;
-    // while (heap.empty() == false) {
-    //     Node node = heap.top();
-    //     cout << node.data << ": " << node.frequency << endl;
-    //     heap.pop();
-    // }
 
     // Build Huffman Tree:
-    Node root = build_tree(heap);
-    printTree(root);
+    Node *root = build_tree(heap);
+    create_codes(root);
 }
